7c7960ac38a99bad05b442e26b054d6bcd6e56a8
Gabriel Vazquez <gabrielv@sonatype.com>
2024-08-12 18:06:23 -0300

    making it work

--- src/jar/WordPrinter.java
+++ src/jar/WordPrinter.java
@@ -1,9 +1,7 @@
-// File: WordPrinter.java
 import java.util.Arrays;
 import java.util.List;
 
-public class // File: MANIFEST.MF
-Main-Class: WordPrinter {
+public class WordPrinter {
     public static void main(String[] args) {
         List<String> words = Arrays.asList("Hello", "World", "This", "Is", "A", "Test");
         for (String word : words) {
