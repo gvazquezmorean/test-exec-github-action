9a8a64fed67d50badcdac17a7c490393fdd5c589
Gabriel Vazquez <gabrielv@sonatype.com>
2024-08-13 09:57:51 -0300

    making maven project

--- src/main/WordPrinter.java
+++ src/main/WordPrinter.java
@@ -1,11 +1,15 @@
 import java.util.Arrays;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WordPrinter {
+    private static final Logger logger = LoggerFactory.getLogger(WordPrinter.class);
+
     public static void main(String[] args) {
         List<String> words = Arrays.asList("Hello", "World", "This", "Is", "A", "Test");
         for (String word : words) {
-            System.out.println(word);
+            logger.info(word);
         }
     }
 }
